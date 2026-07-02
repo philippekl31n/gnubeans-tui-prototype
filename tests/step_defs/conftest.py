8b8923b10a85db77508d4623cc2d9052bb774b77
PhilippeKlein <philippe@birdbird.xyz>
2026-07-02 02:27:08 +0100

    chore: remove non-documentation files, isolate docs

--- tests/step_defs/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-
-@pytest.fixture
-def initial_state():
-    from tests.fixtures.storyboard import make_config, make_mappings
-    from mapping_resolution_tui.reducer import make_initial_state
-    return make_initial_state(make_config(), make_mappings(), frame_height=15)
